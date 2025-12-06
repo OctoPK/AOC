4d3a3d0a788efcfdf2cc9f53e24b3ce89a2a2deb
OctoPK <armand@biguey.com>
2025-12-06 15:08:42 +0100

    add d2 part two

--- d2/d2.py
+++ d2/d2.py
@@ -18,3 +18,26 @@ with open("input.txt") as f:
                 total += i
 
 print("part one :",total)
+
+# part two
+
+total = 0
+
+def is_invalid_id_part_two(n):
+    s = str(n)
+    L = len(s)
+    for i in range(1, L//2+1):
+        motif = s[:i]
+        if motif * (L//i) == s:
+            return True
+    return False
+
+with open("input.txt") as f:
+    ranges = f.readline().strip().split(",")
+    for r in ranges:
+        start, end = map(int, r.split("-"))
+        for i in range(start, end+1):
+            if is_invalid_id_part_two(i):
+                total += i
+
+print("part two :",total)
